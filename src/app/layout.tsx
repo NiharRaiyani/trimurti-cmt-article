0ee46e9deebe10f0fb72fd854be2598a09b3bb83
NiharRaiyani <niharraiyani@gmail.com>
2026-06-26 15:16:39 +0530

    fix: layout title discription

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ const jetbrainsMono = JetBrains_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "Trimurti",
-  description: "Boundary protection systems and precast solutions.",
+  title: "Trimurti Cement Article",
+  description: "We specialize in the manufacturing and installation of RCC precast compound walls, security cabins, portable cabins, prefabricated rooms, boundary fencing systems, labour quarters, site offices, and customized ready-made structures across residential, commercial, and industrial sectors.",
 }
 
 export default function RootLayout({
